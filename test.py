3795fcdaa2a34a6097ad8d648c7bde9e0feb2fbd
Allen White <awhite23@rocketmail.com>
2015-11-14 22:09:38 -0500

    accounting for new variables

--- test.py
+++ test.py
@@ -13,13 +13,25 @@ urlBase 		= 'https://api.spark.io/v1/devices/'
 access_token 	= '25dfc450c2733a52989c60ddc96c0944e45f54c1'
 deviceID 		= '1f0036000147343337373738'
 getFunc1 		= 'analog_val_1'
-s 				= 'sessionArray'
+getFunc2 		= 'analog_val_2'
+s1 				= 'sessionArray1'
+s2				= 'sessionArray2'
 
 dataPointsToRetrieve = 15
 
 
-def getCodeFromSensor():
+def getForFunc1():
 	fullUrl = urlBase + deviceID + '/' + getFunc1 + '/?access_token=' + access_token 
+	return getCodeFromSensor(fullUrl)
+
+
+def getForFunc2():
+	fullUrl = urlBase + deviceID + '/' + getFunc2 + '/?access_token=' + access_token 
+	return getCodeFromSensor(fullUrl)
+
+
+
+def getCodeFromSensor(fullUrl):
 	try:
 		r = requests.get(fullUrl, timeout=2)
 		if r.status_code > 199 and r.status_code < 300:
@@ -34,29 +46,34 @@ def getCodeFromSensor():
 	
 
 def getFullSetOfResults():
-	session[s] = [None] * dataPointsToRetrieve
-	for x in xrange(0,len(session[s])):
-		session[s][x] = getCodeFromSensor()
+	session[s1] = [None] * dataPointsToRetrieve
+	for x in xrange(0,len(session[s1])):
+		session[s1][x] = getCodeFromSensor()
+
+
+	session[s2] = [None] * dataPointsToRetrieve
+	for x in xrange(0,len(session[s2])):
+		session[s2][x] = getCodeFromSensor()
 
 
 
 @app.route('/', methods=['GET'])
 def gimmeResults():
 	getFullSetOfResults()
-	return json.dumps(session[s])
+	return json.dumps({"array1":session[s1], "array2":session[s2]})
 
 
 
 @app.route('/web', methods=['GET'])
 def web():
 	getFullSetOfResults()
-	return render_template('test_index.html', data=session[s])
+	return render_template('test_index.html', data={"array1":session[s1], "array2":session[s2]})
 
 
 @app.route('/gen_large_graph', methods=['GET'])
 def genLargeGraph():
 	getFullSetOfResults()
-	return render_template('large_graph.html', data=session[s])
+	return render_template('large_graph.html', data={"array1":session[s1], "array2":session[s2]})
 
 
 if __name__ == "__main__":
